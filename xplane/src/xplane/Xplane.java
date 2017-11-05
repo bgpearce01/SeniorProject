936e69b5711026dc4fa8507ad3703a55b1603dda
Brian Pearce <bgpearce01@gmail.com>
2017-11-05 15:35:30 -0500

    test

--- xplane/src/xplane/Xplane.java
+++ xplane/src/xplane/Xplane.java
@@ -15,7 +15,7 @@ public class Xplane {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
